742c9ad97911600ae32540e4932dd93d1a24a5c2
Ian Jenkins <ian.jenkins@boxuk.com>
2021-08-05 14:55:13 +0100

    [REFACTOR] Turn off composer support on muplugin for now.
    Causes some issues with test classes and things.

--- wp-hook-attributes.php
+++ wp-hook-attributes.php
@@ -16,11 +16,5 @@ if( ! class_exists( WordPressHookAttributes::class ) ){
     require __DIR__ . '/vendor/autoload.php';
 }
 
-// Will only attempt to use composer classmap if class map authoritative is in use.
-$composerClassLoaders = array_filter(spl_autoload_functions(), fn($entry) => $entry[0] instanceof \Composer\Autoload\ClassLoader);
-/** @var \Composer\Autoload\ClassLoader $composerClassLoader */
-$composerClassLoader = $composerClassLoaders[0][0] ?? null;
-$hasOptimisedAutoloader = $composerClassLoader instanceof \Composer\Autoload\ClassLoader ? $composerClassLoader->isClassMapAuthoritative() : false;
-
-$useComposerClassmap = $hasOptimisedAutoloader;
-(new WordPressHookAttributes())($useComposerClassmap);
+// Composer support is experimental for now so turn off for now.
+(new WordPressHookAttributes())(false);
