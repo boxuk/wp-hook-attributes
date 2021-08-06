66bb2e05073d6cf70609b46e6e96a40c16cfda93
Ian Jenkins <ian.jenkins@boxuk.com>
2021-08-06 11:55:28 +0100

    [SETUP] Ignore note annotation in tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,3 +7,4 @@ use Doctrine\Common\Annotations\AnnotationReader;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 AnnotationReader::addGlobalIgnoredName('dataprovider'); // Needed to stop phpunit errors (case sensitivity issue).
+AnnotationReader::addGlobalIgnoredName('note'); // Needed to stop phpunit errors (case sensitivity issue).
